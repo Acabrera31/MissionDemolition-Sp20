4ecaeff1c29f0610c05fd6750b76a47f45b58a7c
Acabrera31 <Acabrera_@outlook.com>
2020-02-15 18:01:25 -0500

    finished product

--- Assets/scripts/FollowCam.cs
+++ Assets/scripts/FollowCam.cs
@@ -18,19 +18,41 @@ public class FollowCam : MonoBehaviour
     void FixedUpdate()
     {
         // if there's only one line following an if, it doesn't need braces
-        if (POI == null) return; // return if there is no poi // b
+        // if (POI == null) return; // return if there is no poi // b
+        // Get the position of the poi
+        // Vector3 destination = POI.transform.position;
+        Vector3 destination;
+        if (POI == null) { destination = Vector3.zero; }
+
+        else
+        {
             // Get the position of the poi
-                   Vector3 destination = POI.transform.position;
-        //Limit the X & Y to minimum values
-        destination.x = Mathf.Max(minXY.x, destination.x);
-        destination.y = Mathf.Max(minXY.y, destination.y);
-        //Interpolate from the current Camera position toward desintation
-        destination = Vector3.Lerp(transform.position, destination,easing);
-        // Force destination.z to be camZ to keep the camera far enough away//
-        destination.z = camZ;
-        // Set the camera to the destination
-        transform.position = destination;
-        //Keep ground in view
-        Camera.main.orthographicSize = destination.y + 10;
+            destination = POI.transform.position;
+            // If poi is a Projectile, check to see if it's at rest
+            if (POI.tag == "Projectile")
+            {
+                // if it is sleeping (that is, not moving)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // return to default view
+                    POI = null;
+                    // in the next update
+                    return;
+                }
+            }
+        }
+
+            //Limit the X & Y to minimum values
+            destination.x = Mathf.Max(minXY.x, destination.x);
+            destination.y = Mathf.Max(minXY.y, destination.y);
+            //Interpolate from the current Camera position toward desintation
+            destination = Vector3.Lerp(transform.position, destination,
+    easing);
+            // Force destination.z to be camZ to keep the camera far enough away//
+            destination.z = camZ;
+            // Set the camera to the destination
+            transform.position = destination;
+            //Keep ground in view
+            Camera.main.orthographicSize = destination.y + 10;
+        }
     }
-}
--- Assets/scripts/Slingshot.cs
+++ Assets/scripts/Slingshot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Slingshot  :  MonoBehaviour
 {
+    static private Slingshot S;
     // fields set in the Unity Inspector pane
     [Header("Set in Inspector")] // a
     public GameObject prefabProjectile;
@@ -19,8 +20,18 @@ public class Slingshot  :  MonoBehaviour
 
     private Rigidbody projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
@@ -86,6 +97,8 @@ Input.mousePosition; // c
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.ShotFired(); // a
+            ProjectileLine.S.poi = projectile;
         }
     }
 
